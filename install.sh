450b3cd59bfaba6106136e2219c5ba8ffe90c99a
frale-co <frale-co@student.42.fr>
2015-06-16 15:46:17 +0200

    vim git

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # vim config
 rm -rf $HOME/.vimrc $HOME/.vim
-git clone git@github.com:Geam/config_vim.git $HOME/.vim
+git clone git@github.com:lecorref/config_vim.git $HOME/.vim
 if [[ "$?" -eq 0 ]]; then
 	cd $HOME/.vim
 	git submodule init && git submodule update
